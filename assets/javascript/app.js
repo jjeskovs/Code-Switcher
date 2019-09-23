33618de85f5fad28f8b3694b817514f941658ed7
Kevin Halstead <mrmichael423@icloud.com>
2019-09-23 10:11:59 -0600

    added function for urban dictionary

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+$("#").on("click", function() {
+	var search = $(this).attr("data-name")
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=" + search,
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com",
+			"x-rapidapi-key": "2b790bbd2amshc926815e536ecbep1b9bb5jsn68a7524edf49"
+		}
+	}
+	
+	$.ajax(settings).then(function (response) {
+		console.log(response);
+		var results = response.list
+		console.log(results)
+		var definitionDiv = $("<div>")
+		var definition = results[0].definition
+		console.log(definition)
+		var sounds = results[0].sound_urls[0]
+		console.log(sounds)
+	});
+	definitionDiv.append(sounds)
+	definitionDiv.append(definition)
+	$("#").append(definitionDiv)
+})
+	
+
+
