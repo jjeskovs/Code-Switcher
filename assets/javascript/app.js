7655c4fcb3ee7bd4c55a10d8c30c23fb250551ac
Arielle <ariellcollinsb@gmail.com>
2019-09-23 13:49:43 -0600

    made changes to giphy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,33 @@
-$("#search").on("click", function(event) {
-	$("#urban").empty()
-	event.preventDefault();
-	var search = $("#icon_prefix").val().trim()
-	console.log(search)
-	var settings = {
-		"async": true,
-		"crossDomain": true,
-		"url": "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=" + search + "",
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com",
-			"x-rapidapi-key": "2b790bbd2amshc926815e536ecbep1b9bb5jsn68a7524edf49"
-		}
-	}
+// $("#search").on("click", function(event) {
+// 	$("#urban").empty()
+// 	event.preventDefault();
+// 	var search = $("#icon_prefix").val().trim()
+// 	console.log(search)
+// 	var settings = {
+// 		"async": true,
+// 		"crossDomain": true,
+// 		"url": "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=" + search + "",
+// 		"method": "GET",
+// 		"headers": {
+// 			"x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com",
+// 			"x-rapidapi-key": "2b790bbd2amshc926815e536ecbep1b9bb5jsn68a7524edf49"
+// 		}
+// 	}
 	
-	$.ajax(settings).then(function (response) {
-		// console.log(response);
-		var results = response.list
-		console.log(results)
-		var definitionDiv = $("<div>")
-		var definitionWord = results[0].definition
-		console.log(definitionWord)
-		var word = results[0].word
+// 	$.ajax(settings).then(function (response) {
+// 		// console.log(response);
+// 		var results = response.list
+// 		console.log(results)
+// 		var definitionDiv = $("<div>")
+// 		var definitionWord = results[0].definition
+// 		console.log(definitionWord)
+// 		var word = results[0].word
 		
-		definitionDiv.append("<h3>"  + word + "</h3>")
-		definitionDiv.append("<h5>" + definitionWord + "</h5>")
-		$("#urban").append(definitionDiv)
-	});
-})
+// 		definitionDiv.append("<h3>"  + word + "</h3>")
+// 		definitionDiv.append("<h5>" + definitionWord + "</h5>")
+// 		$("#urban").append(definitionDiv)
+// 	});
+// })
 
 
 
--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,37 +1,51 @@
 
 $("#search").on("click", function (event) {
     event.preventDefault();
-    var search = $("#icon_prefix").val().trim()
+    var userSearch = $("#icon_prefix").val().trim()
+    search(userSearch);
     console.log(search)
 
-    var apiKey = "psvs2Qwoq05mon9BeoB6OeULhaI6jdym";
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&limit=1&q=";
+})
 
-    function search(topic) {
-        $("#gifs-appear-here").empty()
-
-        $.ajax({
-            url: queryURL + topic,
-            method: "GET"
-        }).then(function (response) {
-
-            var results = response.data;
-            var gifDiv = $("<div>");
-            var rating = results[i].rating;
-            var p = $("<p>").text("Rating: " + rating);
-
-            gifDiv.append(p);
+var apiKey = "psvs2Qwoq05mon9BeoB6OeULhaI6jdym";
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&limit=1&q=";
 
 
-            gifImg.attr("src", results[i].images.fixed_width_still.url);
-            gifImg.attr("data-still", results[i].images.fixed_width_still.url);
-            gifImg.attr("data-animated", results[i].images.fixed_width.url);
-            gifImg.attr("data-state", "still");
+function search(topic) {
+    $("#gifs").empty()
 
-            $("#gifs-appear-here").prepend(gifDiv);
-        },
+    $.ajax({
+        url: queryURL + topic,
+        method: "GET"
+    }).then(function (response) {
+       console.log(response);
+       var results = response.data;
+       for (var i = 0; i < results.length; i++) {}
     
-            $("#gifs-appear-here img").on("click", function () {
+       var gifDiv = $("<div class='card grey lighten-3'>");
+       var p = $(`<p>"Rating: "${results[i].rating}<p>`);
+       var gifImg = $("<div class='card-image'>");
+       var gifTitle = $("<span class='card-title'>"); 
+        gifDiv.append(p)
+
+
+        // <div class="card grey lighten-3">
+        //                 <div class="card-image">
+        //                     <div id="gif-goes-here"></div>
+        //                     <span class="card-title">Giphy</span>
+        //                 </div>
+        //             </div>
+
+
+        gifImg.attr("src", results[i].images.fixed_width_still.url);
+        gifImg.attr("data-still", results[i].images.fixed_width_still.url);
+        gifImg.attr("data-animated", results[i].images.fixed_width.url);
+        gifImg.attr("data-state", "still");
+
+        $("#gifs").prepend(gifDiv);
+    },
+
+        $("#gifs img").on("click", function () {
 
             var state = $(this).attr("data-state");
             var stillUrl = $(this).attr("data-still");
